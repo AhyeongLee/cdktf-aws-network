5c6c660ce12f79926fd6f52b08ac3de85d8e9dcd
ahyeong <aylee618@gmail.com>
2022-03-25 16:50:05 +0900

    주석 추가

--- constructs/network/aws_route_table.ts
+++ constructs/network/aws_route_table.ts
@@ -10,17 +10,26 @@ export interface AwsRouteTableConfig {
 
 export class AwsRouteTable extends Construct {
   public readonly resource: RouteTable;
+  /**
+   * @param scope
+   * @param resourceCode - 기본 "RT"
+   * @param subnetId - RT에 Subnet을 연결하기 위한 id
+   * @param config
+   * @param tags
+   */
   constructor(scope: Construct, resourceCode = "RT", subnetId: string, config: AwsRouteTableConfig, tags: { [key: string]: string }) {
     const zone: string = config.availabilityZone.substr(-1).toUpperCase();
     const rtTags = JSON.parse(JSON.stringify(tags));
     rtTags.Name = `${tags["Project"]}-${tags["Stage"]}-${resourceCode}-${config.usage}-${zone}`;
     super(scope, rtTags.Name);
 
+    // Route Table 생성
     this.resource = new RouteTable(this, rtTags.Name, {
       route: config.route,
       vpcId: config.vpcId,
       tags: rtTags,
     });
+    // 생성한 Route Table에 Subnet 연결
     new RouteTableAssociation(this, `${rtTags.Name}-ASS`, {
       routeTableId: this.resource.id,
       subnetId,
